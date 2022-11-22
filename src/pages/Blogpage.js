0b8eb242ede544ff2ab997ba6a88af1f5d4281bc
balausa <52880784+balausa@users.noreply.github.com>
2022-11-22 15:14:13 +0600

    useSearchParams

--- src/pages/Blogpage.js
+++ src/pages/Blogpage.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
+import BlogFilter from '../components/BlogFilter';
 
 const Blogpage = () => {
   const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const postQuery = searchParams.get('post') || '';
+  const latest = searchParams.has('latest');
+
+  const startsFrom = latest ? 80 : 1;
+
+
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -13,9 +22,16 @@ const Blogpage = () => {
   return (
     <div>
       <h1>Our news</h1>
+      <BlogFilter 
+      postQuery={postQuery} 
+      latest={latest} 
+      setSearchParams={setSearchParams}
+      />
       <Link to="/posts/new">Add new post</Link>
       {
-        posts.map(post => (
+        posts.filter(
+          post => post.title.includes(postQuery) && post.id >= startsFrom
+        ).map(post => (
           <Link key={post.id} to={`/posts/${post.id}`}>
             <li>{post.title}</li>
           </Link>
